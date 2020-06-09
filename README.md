343b2f82798d318320fc1976fe7630edb4361fcb
AdrianDiLena <adrian.dilena@gmail.com>
2020-06-09 01:46:46 -0400

    added esp code + project description

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # RAINMAKER
+
+This is a weather dashboard and automated irrigation system. 
+It only waters the garden when the probability of rain is low.
+
+The Dashboard is a little bit of an extra frill and somewhat 
+unnecessary, but a way to visualize some weather info and 
+inficate what the system is doing. 
+
+Every morning at 8:00am the program runs and checks the weather.
+If it has a low probability of rain and a high temperature, 
+rainmaker.py will send an MQTT msg to turn on the 1 or more 
+solenoid valves. The valves/relays are controlled by a ESP32 or
+ESP8266 running micropython. 
+
+Then at 8:00pm rainmaker.py will run again. If another low 
+probability of rain and a high temperature forecast, a MQTT 
+msg is sent, turning on the valves connect to the ESPboard. 
+
+This is undoubtedly not going to be perfect.
+
+I am also quite positive that this will not save water... yet.
+But with tweaking and getting a better understanding the 
+nuances of the weather api (currently using darksky, but need
+to change as darksky was sold to apple and ending the API 
+service in 2021), the system should be able to get better 
+results. 
+
+Currently this is being tested on a mixed veggie and
+ornamental not-too-serious garden. But eventually you could 
+adjust it to water based on the demands of different crops as
+long as you planted zones and associated specific relays to
+those zones. 
+
+In a serious vegetable garden where you could quantify the 
+water you put into the system in terms of the vegetables
+you grow, it could be extremely beneficial. 
+
+
--- esp32/main.py
+++ esp32/main.py
@@ -1,14 +1,72 @@
-import time
+from umqtt.robust import MQTTClient
+import dht
 import machine
-import urequests
+import utime
+import webrepl
+
+webrepl.start()
+
+client_id = '1223445'
+mqtt_server = '192.168.2.67'
+topic_sub = b'RAINMAKER/relays/#'
+
+print('\n\nRAINMAKER\nVersion Delta - 6/9/2020\nThreat Level: Midnight\n\nA Weather Sensitive Irrigation System\n')
+
+client = MQTTClient(client_id, mqtt_server)
+client.connect()
 
 led = machine.Pin(2, machine.Pin.OUT)
-led.on()
+valve1 = machine.Pin(12, machine.Pin.OUT) # relay 1
+valve2= machine.Pin(5, machine.Pin.OUT) # relay 2 
+valve3 = machine.Pin(13, machine.Pin.OUT) # relay 3 
+valve4 = machine.Pin(14, machine.Pin.OUT) # relay 4 
 
+pins = [valve1, valve2, valve3, valve4, led]
+for i in pins[:]:
+    i.on()
 
-"""
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if topic == b'RAINMAKER/relays/valve_one' and msg == b'on':
+        valve1.off()      
+    elif topic == b'RAINMAKER/relays/valve_one' and msg == b'off':
+        valve1.on()
+    if topic == b'RAINMAKER/relays/valve_two' and msg == b'on':
+        valve2.off()      
+    elif topic == b'RAINMAKER/relays/valve_two' and msg == b'off':
+        valve2.on()
+    if topic == b'RAINMAKER/relays/valve_three' and msg == b'on':
+        valve3.off()      
+    elif topic == b'RAINMAKER/relays/valve_three' and msg == b'off':
+        valve3.on()
+    if topic == b'RAINMAKER/relays/valve_four' and msg == b'on':
+        valve4.off()      
+    elif topic == b'RAINMAKER/relays/valve_four' and msg == b'off':
+        valve4.on()
 
-Simple relay control via umqtt
+def connect_and_subscribe():
+    global client_id, mqtt_server, topic_sub
+    client = MQTTClient(client_id, mqtt_server)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(topic_sub)
+    print('Connected to: %s \nSubscribed to: %s' % (mqtt_server, topic_sub))
+    return client
 
-"""
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+
+try:
+    client = connect_and_subscribe()
+except OSError as e:
+    restart_and_reconnect()
+
+while True:
+    try:
+        client.check_msg()
+    except OSError as e:
+        restart_and_reconnect()
+
 
